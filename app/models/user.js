76a99c9c9ef42b0e0ea1dcdd7bf0e0525cc2968d
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2019-11-22 00:22:02 -0500

    Added a User model, with a correspondent migration, with the desired fields and options

---
+++ app/models/user.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+  }, {});
+  return User;
+};

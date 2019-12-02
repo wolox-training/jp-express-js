bbd133859b31bd3edf1955191ace4d36b34ac196
Juan Pablo Rojas <juanpablo.rojas@wolox.co>
2019-12-02 12:34:12 -0500

    Changed user response when its created

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -3,9 +3,6 @@ const { signUp } = require('../services/users');
 exports.signUp = (req, res, next) => {
   const { firstName, lastName, email, password } = req.body;
   signUp(firstName, lastName, email, password)
-    .then(user => {
-      res.status(201).send(`A new User was created!
-        firstName: '${user.firstName}', lastName: '${user.lastName}', email: '${user.email}'`);
-    })
+    .then(user => res.status(201).send({ user }))
     .catch(next);
 };
